3eda4566da2f12955aa0f3ae313d8f2dbadebfde
Qingmu Deng <31523537+QingmuDeng@users.noreply.github.com>
2018-09-04 17:22:17 -0400

    bash file for opening gdb and openocd

---
+++ opengdb.sh
@@ -0,0 +1,1 @@
+sudo ~/opt/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/7.3.1-1.1.1/.content/bin/arm-none-eabi-gdb
---
+++ openocd.sh
@@ -0,0 +1,1 @@
+sudo ~/opt/xPacks/@gnu-mcu-eclipse/openocd/0.10.0-8.1/.content/bin/openocd -f board/stm32f4discovery.cfg
